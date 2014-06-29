45a33a268328ee9715638806a4d49b4fd4734cfa
Rostepher <rostepher.dev@gmail.com>
2014-06-29 16:14:44 -0400

    Resturctured scr, now all distance metrics are in src/distance and all phontic algorithms are in src/phonetic.

--- src/distance/damerau_levenshtein.c
+++ src/distance/damerau_levenshtein.c
@@ -14,7 +14,7 @@ int damerau_levenshtein(const char *str1, const char *str2)
     assert(str1 != NULL);
     assert(str2 != NULL);
 
-    // initialize size and counter variables
+    // calculate size of strings
     int str1_len = strlen(str1);
     int str2_len = strlen(str2);
 
@@ -22,14 +22,14 @@ int damerau_levenshtein(const char *str1, const char *str2)
     // while (str1_len > 0 && str2_len > 0 && EQ(str1[0], str2[0]))
     //     str1++, str2++, str1_len--, str2_len--;
 
-    // handle cases where one string is empty
-    if (!str1_len) return str2_len;
-    if (!str2_len) return str2_len;
+    // handle cases where one or both strings are empty
+    if (str1_len == 0)
+        return (str2_len == 0) ? 0 : str2_len;
 
     // initialize matrix to hold distance values
-    int **matrix = malloc((str1_len + 1) * sizeof(int*));
+    unsigned **matrix = malloc((str1_len + 1) * sizeof(unsigned *));
     for (x = 0; x <= str1_len; x++)
-        matrix[x] = malloc((str2_len + 1) * sizeof(int));
+        matrix[x] = malloc((str2_len + 1) * sizeof(unsigned));
 
     // set all the starting values and add all characters to the dict
     matrix[0][0] = 0;
